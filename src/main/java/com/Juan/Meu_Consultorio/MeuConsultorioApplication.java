9a0dc6f8702708d35c445c443ed85d37faddb08a
Juan Pablo Stele Marambio Martinez <juan@Juans-MacBook-Air.local>
2026-08-15 15:47:31 -0300

    create and instal the dependency

---
+++ src/main/java/com/Juan/Meu_Consultorio/MeuConsultorioApplication.java
@@ -0,0 +1,13 @@
+package com.Juan.Meu_Consultorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuConsultorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuConsultorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Juan/Meu_Consultorio/MeuConsultorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Juan.Meu_Consultorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuConsultorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
